bce73f95d8f9f0edb525e131d760717749440d60
BelalAbandah <belalabandah@gmail.com>
2025-05-01 21:59:25 +0300

    D0-Setting Database

---
+++ backend/models/db.js
@@ -0,0 +1,11 @@
+const mongose= require("mongoose")
+
+mongose.connect("mongodb://localhost:27017//Project#4")
+.then((result)=>{
+    console.log("DB is Ready");
+    
+})
+.catch((error)=>{
+    console.log(`error in DB ${error}`);
+    
+})
